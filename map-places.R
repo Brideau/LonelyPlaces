37751ef7c457607d50794430c4ec9834a3451708
Ryan Brideau <ryan.brideau@gmail.com>
2013-12-21 17:10:20 -0400

    All mapping uses Google API

--- map-places.R
+++ map-places.R
@@ -6,9 +6,6 @@ library(mapdata)
 library(geosphere)
 library(ggmap)
 
-# Map-specific variables
-latFocus = c(50, 75) # (lat0, lat1) where the Albers projection is accurate
-lineColours = c("#ffffff", "red")
 fileName = "_CanadaData/CanadaHospitalComplete.csv"
 
 getLineColor <- function(val) {
@@ -33,24 +30,18 @@ location$state <- latlong2state(data.frame(location$lng, location$lat))
 location$nearstate <- latlong2state(data.frame(location$lngnear, location$latnear))
 location <- na.omit(location)
 
-# Draw a base map. Albers is true scale at lat0, lat1
-map("worldHires", "Canada", proj="albers", param=latFocus, col="#999999", fill=FALSE, bg="#ffffff", lwd=0.8)
-
-# Plot the grid points
-pts0 <- mapproject(unlist(location[, 'lng']), unlist(location[, 'lat']))
-pts1 <- mapproject(unlist(location[, 'lngnear']), unlist(location[, 'latnear']))
-seg.colors <- sapply(location$dist_miles, FUN=getLineColor)
-
-# Draw the map and line segments
-segments(pts0$x, pts0$y, pts1$x, pts1$y, col=seg.colors, lwd=1.2)
-
-stateMap <- function(bbox, thedata) {
-  basemap <- get_map(location=bbox, zoom=7, source='google', maptype="roadmap", color="bw")
-  ggmap(basemap) + geom_segment(aes(x=lng, xend=lngnear, y=lat, yend=latnear, color=dist_miles), size=0.6, data=thedata) + geom_point(aes(x=lngnear, y=latnear), size=2, color="#000000", border="black", data=thedata) + scale_color_gradient(low="white", high="red", limits=c(0, 80))
+createMap <- function(bbox, thedata, map.zoom) {
+  basemap <- get_map(location=bbox, zoom=map.zoom, source='google', maptype="roadmap", color="bw")
+  ggmap(basemap) + geom_segment(aes(x=lng, xend=lngnear, y=lat, yend=latnear, color=dist_miles), size=0.6, data=thedata) + geom_point(aes(x=lngnear, y=latnear), size=2, color="#000000", border="black", data=thedata) + scale_color_gradient(low="blue", high="red", limits=c(0, max(thedata$dist_miles)))
 }
 
-# montana <- c(-116.23363635500002, 44.11419853862171, -103.48949572999454, 49.349270649782994)
-# stateMap(montana, subset(location, state=='montana'))
-# 
+# Country bounding box c(left, bottom, right, top)
+canada <- c(-140.920514, 42.016722, -52.524864, 83.2911)
+createMap(canada, location, 3)
+
+new_brunswick <- c(-69.049870, 45.190144, -64.270501, 47.939968)
+createMap(new_brunswick, subset(location, state=='New Brunswick'), 7)
+
+# You can also submit multiple states at once:
 # nv.ut.ca.az <- c(-124.23168322999285, 31.460280893111246, -108.14769885499943, 42.32097191215088)
 # stateMap(nv.ut.ca.az, subset(location, state=='nevada' | state == 'utah' | state == 'california' | state == 'arizona'))
