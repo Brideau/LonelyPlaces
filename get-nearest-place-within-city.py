4264be7c94f58df40c4246c540963cc61b7eaab4
Ryan Brideau <ryan.brideau@gmail.com>
2013-12-22 02:11:05 -0400

    Works on small geographies

--- get-nearest-place-within-city.py
+++ get-nearest-place-within-city.py
@@ -2,7 +2,7 @@
 # types of places and retrieves the nearest
 # one to a grid of points.
 
-from createcitieslist import cities_list
+from creategrid import create_area_grid
 from threading import Lock
 from createthreads import create_threads
 from outputnearest import output_nearest_place
@@ -24,15 +24,16 @@ increment = 0.0035961575091  # 400 m
 
 # places = ['grocery_or_supermarket', 'art_gallery', 'library']
 # places = ['art_gallery', 'library']
-places = ['grocery_or_supermarket', 'hospital', 'art_gallery', 'library']
+places = ['grocery_or_supermarket']
 
 # Print CSV header
 print('place, lat, lng, latnear, lngnear, dist_km')
 
-# I got my centroid list from here: http://geocoder.ca/?freedata=1
-cities = cities_list("library/geodata/canada_cities.csv")
+# Gets the grid of points in the area to check
+area_grid = create_area_grid(lat_NW, lng_NW, lat_SE,
+                             lng_SE, increment=increment)
 
 # Has the grid split up into threads and processed
 for place in places:
-    create_threads(output_nearest_place, cities, place, print_lock,
+    create_threads(output_nearest_place, area_grid, place, print_lock,
                    increment=increment, numthreads=10)
