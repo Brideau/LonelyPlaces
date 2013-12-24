6be806e3be45db1137f0771a8e453dbd31737754
Ryan Brideau <ryan.brideau@gmail.com>
2013-12-24 12:38:52 -0400

    Added timer

--- get-nearest-place.py
+++ get-nearest-place.py
@@ -10,13 +10,15 @@ from threading import Lock, Thread
 from googleapikey import GOOGLE_API_KEY
 from distancesphere import distance_on_unit_sphere
 from generatesearchlocations import create_area_grid, cities_list
+from progressbar import Percentage, Bar, ProgressBar, \
+    ETA
 
 # Prevents write issues from threading
 print_lock = Lock()
 
-scan_type = "grid"  # grid/citylist
-scale = "small"  # small/large: the size of the area being scanned
-if scan_type == "list":
+scan_type = "citylist"  # grid/citylist
+scale = "large"  # small/large: the size of the area being scanned
+if scan_type == "citylist":
     list_name = "library/geodata/canada_cities.csv"
 
 # Places to scan
@@ -24,6 +26,31 @@ places = ['grocery_or_supermarket', 'library']
 # places = ['grocery_or_supermarket', 'art_gallery', 'library']
 # places = ['art_gallery', 'library']
 # places = ['grocery_or_supermarket', 'hospital', 'art_gallery']
+
+if scan_type == "grid":
+    # New Brunswick
+    # lat_NW = 47.939968
+    # lng_NW = -69.049870
+    # lat_SE = 45.190144
+    # lng_SE = -64.270501
+
+    # Canada
+    lat_NW = 83.2911
+    lng_NW = -140.920514
+    lat_SE = 42.016722
+    lng_SE = -52.524864
+
+    # USA
+    # lat_NW = 49.44098806129775
+    # lng_NW = -127.13476612499217
+    # lat_SE = 23.725012
+    # lng_SE = -61.347656
+
+    # Fredericton
+    # lat_NW = 45.998931
+    # lng_NW = -66.748867
+    # lat_SE = 45.870202
+    # lng_SE = -66.550709
 
 if scale == "small":
     increment = 0.0035961575091  # 400m
@@ -58,6 +85,7 @@ def process_grid_sample(functionToThread, grid_sample, poi):
 def output_nearest_place(latitude, longitude, poi):
     """ Given a lat, long and place of interest,
     it prints the nearest place """
+    global progress
     curr_location = str(latitude) + "," + str(longitude)
 
     incrementm = distance_on_unit_sphere(increment, 0, 0, 0)
@@ -97,6 +125,8 @@ def output_nearest_place(latitude, longitude, poi):
             print(poi + ',' + curr_location + ','
                   + str(nearest_lat) + ',' + str(nearest_lng)
                   + ',' + str(least_miles))
+            progress += 1
+            progress_bar.update(progress)
         finally:
             print_lock.release()
     # Or, no location found
@@ -104,37 +134,18 @@ def output_nearest_place(latitude, longitude, poi):
         print_lock.acquire()
         try:
             print(poi + ',' + curr_location + ',,,')
+            progress += 1
+            progress_bar.update(progress)
         finally:
             print_lock.release()
 
-
 if scan_type == "grid":
-    # New Brunswick
-    # lat_NW = 47.939968
-    # lng_NW = -69.049870
-    # lat_SE = 45.190144
-    # lng_SE = -64.270501
-
-    # Canada
-    # lat_NW = 83.2911
-    # lng_NW = -140.920514
-    # lat_SE = 42.016722
-    # lng_SE = -52.524864
-
-    # USA
-    # lat_NW = 49.44098806129775
-    # lng_NW = -127.13476612499217
-    # lat_SE = 23.725012
-    # lng_SE = -61.347656
-
-    # Fredericton
-    lat_NW = 45.998931
-    lng_NW = -66.748867
-    lat_SE = 45.870202
-    lng_SE = -66.550709
-
     # Gets the grid of points in the area to check
     area_grid = create_area_grid(lat_NW, lng_NW, lat_SE, lng_SE, increment)
+
+    progress_bar = (ProgressBar(widgets=[Percentage(), Bar(),
+                    ETA()], maxval=(len(area_grid)*len(places))).start())
+    progress = 0
 
     # Has the grid split up into threads and processed
     for place in places:
@@ -143,5 +154,12 @@ else:
     # List of city centroids
     cities = cities_list(list_name)
     # Has the grid split up into threads and processed
+
+    progress_bar = (ProgressBar(widgets=[Percentage(), Bar(),
+                    ETA()], maxval=(len(cities)*len(places))).start())
+    progress = 0
+
     for place in places:
         create_threads(output_nearest_place, cities, place, numthreads=10)
+
+progress_bar.finish()
